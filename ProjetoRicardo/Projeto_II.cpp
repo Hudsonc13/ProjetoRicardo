8984f2bb511517f7895d4b61fb60a400026a0617
Hudson Clementino <hudson24m@gmail.com>
2024-05-20 09:05:55 -0300

    Sistema de presença adicionado

--- ProjetoRicardo/Projeto_II.cpp
+++ ProjetoRicardo/Projeto_II.cpp
@@ -6,15 +6,10 @@ struct conta {
 	string nome;
 	string email;
 	string numero_inscricao;
+	int presenca;
 	conta* ant;
 	conta* prox;
 
-};
-
-struct desc {
-	conta* ini;
-	conta* fim;
-	int tam;
 };
 
 struct atv {
@@ -22,25 +17,48 @@ struct atv {
 	string tipo;
 	string date;
 	atv* prox;
-	atv* ant;
+};
+
+struct desc_conta {
+	conta* ini;
+	conta* fim;
+	int tam;
+};
+
+struct desc_atv {
+	atv* ini;
+	int tam;
 };
 
 
+
+
 //Função auxiliar que reserva/preenche os espaços de memoria que serão usados para as informações de cada usuário. 
-conta *criar_conta(string nome, string email, string insc) {
+conta* criar_conta(string nome, string email, string insc) {
 	conta* novo = new conta;
 	novo->nome = nome;
 	novo->email = email;
 	novo->numero_inscricao = insc;
+	novo->presenca = NULL;
 	novo->ant = NULL;
+	novo->prox = NULL;
+
+	return novo;
+};
+
+atv* criar_atv(string nome, string email, string insc) {
+	atv* novo = new atv;
+	novo->titulo = nome;
+	novo->tipo = email;
+	novo->date = insc;
 	novo->prox = NULL;
 
 	return novo;
 }
 
 //Função auxiliar que reserva o espaço de memoria do descritor.
-desc* criar_desc() {
-	desc* novo = new desc;
+desc_conta* criar_desc_conta() {
+	desc_conta* novo = new desc_conta;
 	novo->ini = NULL;
 	novo->fim = NULL;
 	novo->tam = 0;
@@ -48,10 +66,18 @@ desc* criar_desc() {
 
 }
 
+desc_atv* criar_desc_atv() {
+	desc_atv* novo = new desc_atv;
+	novo->ini = NULL;
+	novo->tam = 0;
+	return novo;
+}
+
+
 //Função auxiliar que adiciona um novo usuário no final da lista.
-void *adicionar(desc*& lista, string nome, string email, string insc) {
+void adicionar_conta(desc_conta*& lista, string nome, string email, string insc) {
 	conta* novo = criar_conta(nome, email, insc);
-	if (lista == NULL) {
+	if (lista->tam == 0) {
 		lista->ini = novo;
 		lista->fim = novo;
 	}
@@ -64,56 +90,147 @@ void *adicionar(desc*& lista, string nome, string email, string insc) {
 
 }
 
+void adicionar_atv(desc_atv*& lista, string titulo, string tipo, string date) {
+	atv* novo = criar_atv(titulo, tipo, date);
+	if (lista->tam == 0) {
+		lista->ini = novo;
+	}
+	else {
+		lista->ini->prox = novo;
+		lista->ini = novo;
+	}
+	lista->tam++;
+
+}
+
 //Função de coleta de dados que utiliza todas as outras funções auxiliares anteriores
-void preencher_conta(desc*& lista) {
+void preencher_conta(desc_conta*& lista) {
 	string nome = " ";
 	string email;
 	string insc;
+	cout << "Informações do usuário" << endl;
 	while (nome != "x") {
 		cout << "Insira seu nome: (Insira 'x' para finalizar)\n";
 		cin >> nome;
-		cout << "Insira seu email:\n";
-		cin >> email;
-		cout << "Insira seu numero de inscrição:\n";
-		cin >> insc;
-		if (nome != "x")
-			adicionar(lista, nome, email, insc);
+		if (nome != "x") {
+			cout << "Insira seu email:\n";
+			cin >> email;
+			cout << "Insira seu numero de inscrição:\n";
+			cin >> insc;
+			adicionar_conta(lista, nome, email, insc);
+		}
+		system("cls");
 
 	}
 
 }
 //OBS: MESCLAR ESSAS DUAS FUNÇÕES POSTERIORMENTE!!!
-void preencher_atv(desc*& lista) {
+void preencher_atv(desc_atv*& lista) {
 	string titulo = " ";
 	string tipo;
 	string data;
+	cout << "Liste as atividades do evento" << endl;
 	while (titulo != "x") {
 		cout << "Insira o nome da atividade: (Insira 'x' para finalizar)\n";
 		cin >> titulo;
-		cout << "Insira o tipo:\n";
-		cin >> tipo;
-		cout << "Insira a data (XX/XX/XX-XX:XX):\n"; //Adicionar date format???
-		cin >> data;
-		if (titulo != "x")
-			adicionar(lista, titulo, tipo, data);
-
+		if (titulo != "x") {
+			cout << "Insira o tipo:\n";
+			cin >> tipo;
+			cout << "Insira a data (XX/XX/XX-XX:XX):\n"; //Adicionar date format???
+			cin >> data;
+			adicionar_atv(lista, titulo, tipo, data);
+		}
+		system("cls");
 	}
 
 }
 
 //Criação de uma matriz dinamica para fazer a contagem da presença dos usuarios nas atividades
-bool** criar_mat(int usuario, int atv) {
-	bool **lin = new bool*[usuario];
-	for (int i = 0; i < usuario; i++) {
-		for (int j = 0; j < atv; j++)
-			lin[i][j] = new bool[atv];
-	}
+bool** criar_mat(desc_conta*& usuarios, desc_atv*& atv) {
+	bool **mat = new bool *[usuarios->tam];
+	for (int i = 0; i < usuarios->tam; i++)
+			mat[i] = new bool[atv->tam];
+	return mat;
 }
+
+
+//Marca a presença dos usuarios
+void presenca(bool** mat, desc_conta *&usuarios, desc_atv *&atividade) {
+	int pres;
+	conta* aux = usuarios->ini;
+	for (int i = 0; i < usuarios->tam; i++){
+		cout << "Usuario: " << usuarios->ini->nome;
+		for (int j = 0; j < atividade->tam; j++) {
+			cout << "Atividade: " << atividade->ini->titulo <<"\n";
+			cout << "1: Presente\n0: Ausente\n";
+			cin >> pres;
+			if (pres == 0)
+				mat[i][j] = false;
+			if (pres == 1)
+				mat[i][j] = true;
+			else {
+				cout << "Valor inserido não condiz com o solicitado!!\n Tente novamente";
+				i--;
+				j--;
+			}
+			system("cls");
+		}
+
+	}
+
+}
+
+
+//Calcula a presença de cada usuario e retorna a porcentagem
+void cal_presenca(bool** mat, desc_conta*& usuario, desc_atv*& atv) {
+	conta* aux = usuario->ini;
+	for (int i = 0; i < usuario->tam; i++) {
+		int cont = 0;
+		for (int j = 0; j < atv->tam; j++) {
+			if (mat[i][j] == true)
+				cont++;
+		}
+		usuario->ini->presenca = cont/atv->tam;
+		aux = usuario->ini->prox;
+		
+	}
+	
+}
+
+void imprimir(desc_conta*& user) {
+	if (user == NULL) {
+		cout << "Nenhum usuário encontrado!";
+
+	}
+	else {
+		conta* aux = user->ini;
+		while (aux != NULL) {
+			cout << "Nome: " << aux->nome;
+			cout << "Email: " << aux->email;
+			cout << "Numero de inscrição: " << aux->numero_inscricao;
+			cout << "Nivel de presença: " << aux->presenca;
+			aux = aux->prox;
+		}
+	}
+
+}
+
 
 
 
 int main() {
 
+	bool** matriz;
+	desc_conta* usuarios = criar_desc_conta();
+	desc_atv* atv = criar_desc_atv(); 
 
+	preencher_atv(atv);
+	preencher_conta(usuarios);
+
+	matriz = criar_mat(usuarios, atv);
+	presenca(matriz, usuarios, atv);
+	cal_presenca(matriz, usuarios, atv);
+
+	imprimir(usuarios);
 
 }
